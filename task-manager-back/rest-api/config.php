6809aaa54a895ddd6c9209497894ca2a5fda5c59
Eorime <elenedgebuadze83@gmail.com>
2025-05-27 15:03:59 +0400

    Setting up config file

---
+++ task-manager-back/rest-api/config.php
@@ -0,0 +1,16 @@
+<?php
+$hostname = 'localhost';
+$username = "root";
+$password = "momo83";
+$database = "taskmanager";
+
+$connection = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$connection) {
+    die("connection failed" . mysqli_connect_error());
+}
+
+if ($connection) {
+    echo ("maladec shechku");
+}
+
