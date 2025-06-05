a519636a546d5275cfee678ce32fcc0d4086c7a8
Eorime <elenedgebuadze83@gmail.com>
2025-06-05 12:24:33 +0400

    Set up front-end files

---
+++ task-manager-front/src/config/routes.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import { routes } from "../constants/routes";
+
+export const appRoutes = [
+	{
+		path: routes.home,
+		Component: "",
+	},
+];
---
+++ task-manager-front/src/constants/routes.js
@@ -0,0 +1,17 @@
+const home = "/";
+const task = "/task/:id";
+const createTask = "/createTask";
+const employees = "/employees";
+const departments = "/departments";
+const signUp = "/signup";
+const logIn = "/login";
+
+export const routes = {
+	home,
+	task,
+	createTask,
+	employees,
+	departments,
+	signUp,
+	logIn,
+};
