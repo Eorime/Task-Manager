4b4e1731bfc6097bbfd12232ecb6fcd9866385dd
Eorime <elenedgebuadze83@gmail.com>
2025-06-12 12:22:40 +0400

    Fix index.php

--- task-manager-back/rest-api/index.php
+++ task-manager-back/rest-api/index.php
@@ -8,22 +8,23 @@ try {
     $tasks = json_decode($tasks, true);
     //task by id
     $taskId = 1;
-    $task = sendRequest($restAPIBaseUrl . "/api.php/tasks/$taskId", "GET");
-    $task = json_decode($task, true);
+    $tasks = sendRequest($restAPIBaseUrl . "/api.php/tasks/$taskId", "GET");
+    $tasks = json_decode($tasks, true);
     //add new task
     $data = [
-        '$task_title' => 'task_title',
-        '$task_desc' => 'task_desc',
+        'task_title' => 'task_title',
+        'task_desc' => 'task_desc',
         //assigned to?
-        '$task_dept' => 'task_dept',
-        '$task_due' => 'task_due',
-        '$task_status' => 'task_status',
-        '$task_priority' => 'task_priority',
-        '$task_created' => 'task_created',
-        '$task_updated' => 'task_updated',
+        'task_dept' => 'task_dept',
+        'task_due' => 'task_due',
+        'task_status' => 'task_status',
+        'task_priority' => 'task_priority',
+        'task_created' => 'task_created',
+        'task_updated' => 'task_updated',
     ];
     $result = sendRequest($restAPIBaseUrl . "/api.php/tasks", "POST", $data);
     $result = json_decode($result, true);
+    echo "<p>" . $tasks . "</p>";
     //update task 
     $taskId = 1;
     $data = [
