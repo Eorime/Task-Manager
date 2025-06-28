9e6a61f7e016e8107ae0738b05f061c58b186760
Eorime <elenedgebuadze83@gmail.com>
2025-06-28 14:06:41 +0400

    Setup pages

--- task-manager-front/src/config/routes.js
+++ task-manager-front/src/config/routes.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
 import { routes } from "../constants/routes";
 import Home from "../pages/homePage/Home";
+import SignUp from "../pages/signUp/SignUp";
 
 export const appRoutes = [
 	{
 		path: routes.home,
 		Component: Home,
+	},
+	{
+		path: routes.signUp,
+		Component: SignUp,
 	},
 ];
