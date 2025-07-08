32d61f32bad9a8f610ef57b482d91babd12673a1
Eorime <elenedgebuadze83@gmail.com>
2025-07-08 13:34:14 +0400

    Fetch employees

--- task-manager-front/src/pages/employees/Employees.jsx
+++ task-manager-front/src/pages/employees/Employees.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./styles";
 import Navigation from "../../components/common/navigation/Navigation";
 
 const Employees = () => {
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		fetch("/rest-api/api.php/employees")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setData(data.employees);
+			});
+	}, []);
+
 	return (
 		<Container>
 			<Navigation />
+			{data &&
+				data.map((employee, index) => <div key={index}> {employee.name}</div>)}
 		</Container>
 	);
 };
